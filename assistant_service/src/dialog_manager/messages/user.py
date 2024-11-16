55a403a656962817f480cc7e9594305309d97d8a
anndvoretskaya <anja.dvr@gmail.com>
2024-11-16 17:26:36 +0100

    Add dialog saving to mongo. Remove open ports from docker-compose. Add missing users to dockerfiles

--- assistant_service/src/dialog_manager/messages/user.py
+++ assistant_service/src/dialog_manager/messages/user.py
@@ -4,9 +4,11 @@ from dialog_manager.models import DialogRoles
 from dialog_manager.utilities import get_hash
 
 
-def get_message_user(content: str, id_msg:str, created_at: datetime, **kwargs) -> dict[str, str | datetime]:
-    return {"id": id_msg,
-            "checksum":get_hash(content=content),
-            "role": DialogRoles.user,
-            "content": content,
-            "created_at": created_at}
+def get_message_user(content: str, id_msg: str, created_at: datetime, **kwargs) -> dict[str, str | datetime]:
+    return {
+        "id": id_msg,
+        "checksum": get_hash(content=content),
+        "role": DialogRoles.user,
+        "content": content,
+        "created_at": created_at
+    }
